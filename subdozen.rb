ba84c7216b31b5fd3553864e0f055337e1df310f
ZenalArifin <39817707+z3n70@users.noreply.github.com>
2021-07-12 18:11:35 +0700

    Update subdozen.rb

--- subdozen.rb
+++ subdozen.rb
@@ -1,6 +1,8 @@
 require 'httparty'
 require 'colorize'
 require 'json'
+require 'uri'
+
 str = <<END
             ================================
 
@@ -25,12 +27,12 @@ if dozen == "1"
     puts  "Example             : "+("google.com").blue
     print "Enter Your Domain   : "
     d = gets.chomp
-    print "Enter Name File.txt : "
+    print "Enter File Name.txt : "
     name_file = gets.chomp
 
     puts
     headers = {
-        "apikey"     => "qLpw4GFx3XVRL4s88t4RQ1xhI9wkNZdg",
+        "apikey"     => "Imq3OntknXmbUGuN3IBOtMQCXn0usDq4",
     }
     response = HTTParty.get("https://api.securitytrails.com/v1/domain/#{d}/subdomains",
     :headers => headers
@@ -44,50 +46,60 @@ if dozen == "1"
     puts
     puts "#{Dir.pwd}/#{name_file}".yellow
     puts
+
 elsif dozen == "2"
 
     puts "Example subdomain-list.txt"
     puts
-    print "Enter List Domain.txt "
+    print "Enter List Domain.txt : "
     file = gets.chomp
+    print "Enter Save File Name : "
+    name_f = "domcheck-#{gets.chomp}.txt"
     puts
     
     data_file = File.open("#{file}", "r").read
     data1 = data_file.split("\n")
+    
+begin 
     data1.each do |line|
-
         newuri = URI::HTTP.build({:host => "#{line}"})
-        response = HTTParty.get("#{newuri}", timeout: 5)
+        response = HTTParty.get("#{newuri}", timeout: 10)
+
             case response.code 
                 when 200
                     puts "OK           <= #{newuri}".yellow
-                    File.open("a.txt", "a+"){|file|file.write("#{newuri} => OK\n")}
+                    File.open("#{name_f}", "a+"){|file|file.write("#{newuri} => OK\n")}
 
                 when 404
-                    puts "Teu Aya!!    <= #{newuri}".blue
-                    # File.open("a.txt", "a+"){|file|file.write("#{newuri} => Not Found\n")}
+                    puts "Not Found!!    <= #{newuri}".blue
+                    # File.open("#{name_f}", "a+"){|file|file.write("#{newuri} => Not Found\n")}
 
                 when 403
                     puts "Forbidden    <= #{newuri}".red
-                    File.open("a.txt", "a+"){|file|file.write("#{newuri} => Forbidden\n")}
+                    File.open("#{name_f}", "a+"){|file|file.write("#{newuri} => Forbidden\n")}
 
                 when 400
                     puts "Bad request  <= #{newuri}".green
-                    File.open("a.txt", "a+"){|file|file.write("#{newuri} => Bad Request\n")}
+                    File.open("#{name_f}", "a+"){|file|file.write("#{newuri} => Bad Request\n")}
 
                 when 500 
                     puts "Server error <= #{newuri}".blue
-                    File.open("a.txt", "a+"){|file|file.write("#{newuri} => Server Error\n")}
+                    File.open("#{name_f}", "a+"){|file|file.write("#{newuri} => Server Error\n")}
 
                 when 408 
                     puts "Time out     <= #{newuri}".blue
-                    File.open("a.txt", "a+"){|file|file.write("#{newuri} => Time Out\n")}
-
+                    File.open("#{name_f}", "a+"){|file|file.write("#{newuri} => Time Out\n")} 
+		
                 when 302 
                     puts "Redirect     <= #{newuri}".green
-                    File.open("a.txt", "a+"){|file|file.write("#{newuri} => Redirect\n")}
-                end
+                    File.open("#{name_f}", "a+"){|file|file.write("#{newuri} => Redirect\n")}   
+                 end
+            end
+        rescue
         end
+	puts
+	puts "#{Dir.pwd}/#{name_f}".yellow
+	puts 
 else
-    puts "Inout Salah Jang!!".yellow
+    puts "Input Salah Jang!!".yellow
 end
