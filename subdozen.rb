63843d418e26765d65d6f44d1d3cbfba9e9637d0
ZenalArifin <39817707+z3n70@users.noreply.github.com>
2023-03-29 04:09:08 +0700

    Update subdozen.rb

--- subdozen.rb
+++ subdozen.rb
@@ -3,15 +3,17 @@
 require 'httparty'
 require 'colorize'
 require 'json'
+require 'open-uri'
 require 'uri'
 
+loop do
 str = <<END
             ================================
 
                ┌─┐┬ ┬┌┐ ┌┬┐┌─┐┌─┐┌─┐┌┐┌
                └─┐│ │├┴┐ │││ │┌─┘├┤ │││
                └─┘└─┘└─┘─┴┘└─┘└─┘└─┘┘└┘
-                         v 0.2
+                         v 1.1
             ================================
 		        @z3n70
 
@@ -20,33 +22,24 @@ puts str.yellow
 puts
 puts "1. Subdomain Scanning"
 puts "2. Status Domain Checker"
+puts "3. Exit"
 puts
 print "Enter Number : "
 dozen = gets.chomp
 puts
 
 if dozen == "1"
-    puts  "Example             : "+("google.com").colorize(:color => :black, :background => :white)
-    print "Enter Your Domain   : "
-    d = gets.chomp
-    print "Enter File Name.txt : "
-    name_file = gets.chomp
+    print "Inputkan Target : "
+    target = gets.chomp
 
     puts
-    headers = {
-        "apikey"     => "UBGkWBn0wVZxzw94OWEnmGWp0n71cSpo",
-    }
-    response = HTTParty.get("https://api.securitytrails.com/v1/domain/#{d}/subdomains",
-    :headers => headers
-    )
-    json_data = JSON.parse(response.body)
-    for domain in json_data['subdomains']
-        out = domain + ".#{d}"
-        puts out
-        File.open("#{name_file}", "a+"){|dom| dom.write("#{out}\n")}
-    end
+    url = "https://crt.sh/?q=%25.#{target}"
+    curl_out = URI.open(url).read
+    domain_list = curl_out.scan(/<TD>(.*?)<\/TD>/).flatten.select {|x| x.match?(/^[\w\d.-]+\.[a-z]{2,}$/)}
+     File.write("subdomain-#{target}", domain_list.uniq.join("\n"))
+    puts File.read("subdomain-#{target}")
     puts
-    puts "#{Dir.pwd}/#{name_file}".yellow
+    puts "File Saved => #{Dir.pwd}/subdomain-#{target}".yellow
     puts
 
 elsif dozen == "2"
@@ -58,7 +51,7 @@ begin
     print "Enter Save File Name : "
     name_f = "domcheck-#{gets.chomp}.txt"
     puts
-    
+
     data_file = File.open("#{file}", "r").read
 
 #debugger
@@ -70,39 +63,39 @@ begin
         response = HTTParty.get(newuri, timeout: 5, :verify => false)
         # puts newuri
 
-        if response.code == 200 
+        if response.code == 200
             puts "#{newuri} => OK".yellow
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => OK\n")}
-        
+
         elsif response.code == 404
             puts "#{newuri} => Not Found!!".red
-            File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Not Found\n")} 
-        
+            File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Not Found\n")}
+
         elsif response.code == 403
             puts "#{newuri} => Forbidden".blue
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Forbidden\n")}
-        
+
         elsif response.code == 500
             puts "#{newuri} => Server Error".red
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Server Error\n")}
-       
+
         elsif response.code == 400
             puts "#{newuri} => Bad Request".blue
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Bad Request\n")}
-       
+
         elsif response.code == 302
             puts "#{newuri} => Redirect".blue
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Redirect\n")}
-       
+
         elsif response.code == 408
             puts "#{newuri} => Time Out".red
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Time Out\n")}
-        
+
         elsif response.code == 521
             puts "#{newuri} => Server Down".red
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Server Down\n")}
 
-        else 
+        else
             puts "#{newuri} => Error".red
             File.open("#{name_f}","a+"){|file|file.write("#{newuri} => Error\n")}
         end
@@ -110,7 +103,7 @@ begin
     rescue OpenSSL::SSL::SSLError
         puts "SSL Error..."
     rescue Errno::ECONNREFUSED
-        puts "Connection Refused!"    
+        puts "Connection Refused!"
 	end
 end
 
@@ -119,7 +112,12 @@ end
     puts
 rescue Interrupt
     puts "Leaving the program...".red
-end 
-else
-    puts "Input Salah Jang!!".yellow
-end 
+end
+
+    elsif dozen == "3"
+    exit 1
+
+    else
+        puts "Wrong Input!".yellow
+    end
+end
